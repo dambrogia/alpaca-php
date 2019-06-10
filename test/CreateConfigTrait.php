b740ba442bfbe1b310e2ada4cede3cc7d8438197
Dominic Dambrogia <dominic.dambrogia@palmettostatearmory.com>
2019-06-09 21:52:37 -0700

    add dotenv to load creds during testing

--- test/CreateConfigTrait.php
+++ test/CreateConfigTrait.php
@@ -2,16 +2,22 @@
 
 namespace Dambrogia\AlpacaTest;
 
+use Dotenv\Dotenv;
 use Dambrogia\Alpaca\Config;
+use Dambrogia\Alpaca\Concern\ConfigException;
 
 trait CreateConfigTrait
 {
     public function createConfig($key = '', $secret = '', $env = Config::ENV_PAPER): Config
     {
-        // @todo - implement dotenv for thest values. throw exception during
-        // test if they don't exist.
-        $key = empty($key) ? 'PKOGCMHJNFJ6J3PRX9B7' : $key;
-        $secret = empty($secret) ? 'qQDCH9xabxm0jCjDaKVFyFajcal3hafgUjCjQ0X9' : $secret;
+        Dotenv::create(__DIR__.'/..')->load();
+
+        $key = empty($key) ? getenv('KEY_ID') : $key;
+        $secret = empty($secret) ? getenv('SECRET_KEY'): $secret;
+
+        if (! $key || ! $secret) {
+            throw new ConfigException('Invalid key/secret or no .env set for testing.');
+        }
 
         return new Config($key, $secret, $env);
     }
